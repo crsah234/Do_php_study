f7d961517ff036e75a2f51e2659da08dbe0860e7
crsah234 <99774336+crsah234@users.noreply.github.com>
2025-01-04 21:52:40 +0900

    Add files via upload

---
+++ php_web_programming/chapter2/hello_css.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <style type="text/css">
+        body{color:red; background: aliceblue};
+
+    </style>
+
+</head>
+<body>
+
+asdasd
+</body>
+
+
+</html>
