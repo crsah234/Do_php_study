84a91283c2450af0d327ab8d48390fb32659b287
crsah234 <99774336+crsah234@users.noreply.github.com>
2025-01-04 20:37:42 +0900

    Create fruit.php

---
+++ php_web_programming/chapter1/fruit.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+
+<form name="fruit" action="./fruit_data.php" method="post">
+    원하는 가격을 입력해주세요<br>
+    가격 : <input type="text" name="low"><br>
+    <input type="submit" value="찾기">
+</form>
+</body>
+
+</html>
